abfefa872ceb68282e329a0dab77032db8419a3b
Joseph Humfrey <joseph.humfrey@gmail.com>
2013-10-29 16:53:30 +0000

    Attempting to re-integrate changes into latest

--- OHAttributedLabel/Source/OHAttributedLabel.h
+++ OHAttributedLabel/Source/OHAttributedLabel.h
@@ -117,4 +117,9 @@ __attribute__((deprecated("You should remove links directly to from NSAttributed
 @property(nonatomic, assign) BOOL centerVertically;
 //! Allows to draw text past the bottom of the view if need. May help in rare cases (like using Emoji)
 @property(nonatomic, assign) BOOL extendBottomToFit;
+
+
+- (int) characterIndexAtPoint:(CGPoint)point;
+- (CGRect) rectOfCharactersInRange:(NSRange)range;
+
 @end
